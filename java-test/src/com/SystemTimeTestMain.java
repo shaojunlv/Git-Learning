a18f3452265556c10617f37a19349ed5880388d4
lvshaojun <lsj581x@gmail.com>
2017-01-09 12:44:58 +0800

    this is modify

--- java-test/src/com/SystemTimeTestMain.java
+++ java-test/src/com/SystemTimeTestMain.java
@@ -1,8 +1,5 @@
 package com;
 
-import java.text.SimpleDateFormat;
-import java.util.Date;
-
 /**
  * Created by lawrence on 2017/1/6.hahha
  * test
@@ -13,13 +10,6 @@ public class SystemTimeTestMain {
     public static void main(String [] arg){
         long currentTime=System.currentTimeMillis();
         long temp=1483667789969L;
-        Date nowData=new Date(currentTime);
-        Date nowData2=new Date(currentTime);
-        SimpleDateFormat simpleDateFormat=new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-        String result=simpleDateFormat.format(nowData);
-        String result2=simpleDateFormat.format(nowData2);
-        System.out.print(result);
-        System.out.print(result2);
     }
 
 }
